82d0a509e9097d55cf8f2bf7a36ba8ccd2dd7af0
Denys Serhiienko <d.sergiienko@mobidev.biz>
2025-12-30 12:21:36 +0200

    rename product consts as local ones and move inside the test

--- tests/productDetails.spec.ts
+++ tests/productDetails.spec.ts
@@ -2,21 +2,21 @@ import { test, expect } from '@playwright/test';
 import { HomePage } from '../pages/home.page';
 import { ProductPage } from '../pages/product.page';
 
-const PRODUCT_NAME = 'Combination Pliers';
-const PRODUCT_PRICE = '14.15'
 
 test('Verify user can view product details', async ({ page }) => {
   test.skip(!!process.env.CI, 'Skipped in CI due to Cloudflare');
   
   const homePage = new HomePage(page);
   const productPage = new ProductPage(page);
+  const productName = 'Combination Pliers';
+  const productPrice = '14.15'
 
   await homePage.open();
-  await homePage.clickProduct(PRODUCT_NAME);
+  await homePage.clickProduct(productName);
 
   await expect(page).toHaveURL(/\/product/);
-  await expect(productPage.productName).toHaveText(PRODUCT_NAME);
-  await expect(productPage.unitPrice).toHaveText(PRODUCT_PRICE);
+  await expect(productPage.productName).toHaveText(productName);
+  await expect(productPage.unitPrice).toHaveText(productPrice);
   await expect(productPage.addToCart).toBeVisible();
   await expect(productPage.addToFavorites).toBeVisible();
 });
