2a205d305b83065cad4d730ce1bade82a9f47876
Denys Serhiienko <d.sergiienko@mobidev.biz>
2026-01-03 17:29:57 +0200

    trying to fix failed CI test

--- tests/auth.login.with.file.spec.ts
+++ tests/auth.login.with.file.spec.ts
@@ -3,7 +3,7 @@ import path from 'path';
 
 const authFile = path.join(__dirname, '../playwright/.auth/user.json');
 
-test.use({storageState: authFile});
+test.use(process.env.CI ? {} : { storageState: authFile });
 
 test('Verify login with valid credentials', async ({ page }) => {
   test.skip(!!process.env.CI, 'Skipped in CI due to Cloudflare');
