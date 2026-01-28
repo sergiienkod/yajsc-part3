28ac637874ed3f519dbea86f0b8295a232c677a0
Denys Serhiienko <d.sergiienko@mobidev.biz>
2026-01-28 13:42:17 +0200

    refactor: replace TEST_USER with USER configuration in loggedInApp fixtures

--- fixtures/loggedInApp.fixtures.ts
+++ fixtures/loggedInApp.fixtures.ts
@@ -1,8 +1,6 @@
 import { test as base } from './app.fixtures';
 import { AllPages } from '../pages/allPages';
-import { TEST_USER } from '../testData/testUser';
-
-const { email, password } = TEST_USER;
+import { USER } from '../config/baseConfig';
 
 
 type LoggedInFixtures = {
@@ -14,7 +12,7 @@ export const test = base.extend<LoggedInFixtures>({
   loggedInApp: async ({ allPages }, use) => {
     
     await allPages.loginPage.goToLoginPage();
-    await allPages.loginPage.performLogin(email, password);
+    await allPages.loginPage.performLogin(USER.email, USER.password);
 
     await allPages.accountPage.waitUntilOpened();
     
